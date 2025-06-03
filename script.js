4e25d42a5c8f3e6c26872ed08ae8cdf6f1c74bba
ashim-sharma-console <ashimsharma49@gmail.com>
2025-06-03 15:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+const speed = document.querySelector('.speed');
+const bar = speed.querySelector('.speed-bar');
+const video = document.querySelector('video');
+
+const min = 0.5;
+const max = 4;
+
+speed.addEventListener('mousemove', function (e) {
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  const playbackRate = percent * (max - min) + min;
+  
+  video.playbackRate = playbackRate.toFixed(2);
+  bar.style.height = `${percent * 100}%`;
+  bar.textContent = `${playbackRate.toFixed(1)}×`;
+});
+
